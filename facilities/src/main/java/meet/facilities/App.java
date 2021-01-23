e49bd560bc72f12863489728a03d5719c3f11f4a
veronicavillegas <urseza@hotmail.com>
2021-01-23 18:47:52 -0300

    Initial commit

---
+++ facilities/src/main/java/meet/facilities/App.java
@@ -0,0 +1,10 @@
+package meet.facilities;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(App.class)
+                .run(args);
+          
+    }
+}
