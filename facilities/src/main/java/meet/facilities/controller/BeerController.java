119e87451a3ef17e83359e678f745c2fd789c925
veronicavillegas <urseza@hotmail.com>
2021-01-29 10:20:00 -0300

    Fix en beer controller

--- facilities/src/main/java/meet/facilities/controller/BeerController.java
+++ facilities/src/main/java/meet/facilities/controller/BeerController.java
@@ -1,6 +1,10 @@
 package meet.facilities.controller;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,16 +43,18 @@ public class BeerController {
             @RequestParam int beersByBox) {
         // Creo dto de user, meet, y whether
         // Llamo al servicio para calcular la cantidad de cervezas
-        Meet meet = getMeet(date, attendants, city, country);
         User user = getUser(emailUser);
         meet.facilities.dto.Beer beer;
 
         try {
+            Meet meet = getMeet(date, attendants, city, country);
             beer = foodService.calculateBeer(meet, user, beersByBox, attendants);
-        } catch(IOException ex) {
-            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
-        } catch(NotFoundWeatherException | InvalidInputDataException ex) {
-            throw new ApiException(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        } catch (IOException ex) {
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), ex.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR.value());
+        } catch (NotFoundWeatherException | InvalidInputDataException | ParseException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage(),
+                    HttpStatus.BAD_REQUEST.value());
         }
 
         return new ResponseEntity<>(modelMapper.map(beer, Beer.class), HttpStatus.OK);
@@ -61,12 +67,13 @@ public class BeerController {
         return user;
     }
 
-    private Meet getMeet(String date, int attendants, String city,
-            String country) {
+    private Meet getMeet(String date, int attendants, String city, String country) throws ParseException {
         Location location = getLocation(city, country);
-
+        Date forestDate = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss").parse(date);
+        
         Meet meet = new Meet();
         meet.setLocation(location);
+        meet.setDate(forestDate);
 
         return meet;
     }
